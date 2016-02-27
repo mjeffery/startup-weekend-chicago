a8c489f373af0d29e5bcb2b0cd1c60aaaea02757
Derek <dereknelson01@gmail.com>
2016-02-27 17:19:11 -0500

    Ng-annotate

--- app/scripts/share.js
+++ app/scripts/share.js
@@ -1,4 +1,4 @@
 angular.module('gift-tapes')
-    .controller('ShareCtrl', function($scope, playlist) {
+    .controller('ShareCtrl', ['$scope', 'playlist', function($scope, playlist) {
         $scope.playlist = playlist;
-    });
+    }]);
