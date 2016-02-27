193dfd307dd558f4813862f1a74239ea443288f4
mjeffery <michael.jeffery@gmail.com>
2016-02-27 12:07:24 -0500

    now it should work on heroku I think

--- app.js
+++ app.js
@@ -20,10 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
