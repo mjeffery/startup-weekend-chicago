3ff7008e67a173f851e385650bba638bca9b79a8
mjeffery <michael.jeffery@gmail.com>
2016-02-27 16:07:14 -0500

    playlist adding rough sketch

--- app/scripts/MusicSearchCtrl.js
+++ app/scripts/MusicSearchCtrl.js
@@ -1,11 +1,25 @@
 angular.module('gift-tapes')
 
 .controller('MusicSearchCtrl', function($scope, soundCloudSearchService) {
+	$scope.playlist = [];
 	$scope.songs = [];
+
 	$scope.search = function(query) {
 		soundCloudSearchService.find(query)
 			.then(function(results) {
-				$scope.songs = results;
+				$scope.songs = _.filter(results, function(song) {
+					return !_.some($scope.playlist, { id: song.id });	
+				});
 			});
 	};
+
+	$scope.add = function(song) {
+		$scope.playlist.push(song);
+	}
+	
+	$scope.remove = function(song) {
+		var idx = _.findIndex($scope.playlist, { id: song.id });
+		if(idx > 0) 
+			$scope.playlist.splice(idx, 1);
+	}
 });
