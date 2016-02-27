d52fb40d36b01d52894d5ad39792c72f7e436366
mjeffery <michael.jeffery@gmail.com>
2016-02-27 16:46:48 -0500

    can add and remove from playlist

--- app/scripts/MusicSearchCtrl.js
+++ app/scripts/MusicSearchCtrl.js
@@ -1,25 +1,35 @@
 angular.module('gift-tapes')
 
-.controller('MusicSearchCtrl', function($scope, soundCloudSearchService) {
+.filter('notInList', function() {
+	return function(arr, otherArr) {
+		if(!angular.isArray(arr)) return arr;
+		if(!angular.isArray(otherArr)) return otherArr;
+
+		return _.filter(arr, function(item) {
+			return !_.some(otherArr, { id: item.id });
+		});
+	}
+})
+
+.controller('MusicSearchCtrl', function($scope, soundCloudSearchService, notInListFilter) {
 	$scope.playlist = [];
 	$scope.songs = [];
 
 	$scope.search = function(query) {
 		soundCloudSearchService.find(query)
 			.then(function(results) {
-				$scope.songs = _.filter(results, function(song) {
-					return !_.some($scope.playlist, { id: song.id });	
-				});
+				$scope.songs = results;
 			});
 	};
 
 	$scope.add = function(song) {
-		$scope.playlist.push(song);
+		if(!_.some($scope.playlist, { id: song.id }))
+			$scope.playlist.push(song);
 	}
 	
 	$scope.remove = function(song) {
 		var idx = _.findIndex($scope.playlist, { id: song.id });
-		if(idx > 0) 
+		if(idx >= 0) 
 			$scope.playlist.splice(idx, 1);
 	}
 });
