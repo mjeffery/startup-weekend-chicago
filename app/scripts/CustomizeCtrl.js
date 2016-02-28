5ca972d607f99880862e006eb39fddb61dade1d6
mjeffery <michael.jeffery@gmail.com>
2016-02-28 14:03:51 -0500

    starting in on that modal

--- app/scripts/CustomizeCtrl.js
+++ app/scripts/CustomizeCtrl.js
@@ -1,5 +1,21 @@
 angular.module('gift-tapes')
 
-.controller('CustomizeCtrl', function($scope) {
+.controller('CustomizeCtrl', function($scope, $uibModal) {
+
+	$scope.choosePhoto = function() {
+		var modal = $uibModal.open({
+			templateUrl: 'templates/modals/choosePhoto.html',
+			controller: 'ChoosePhotoCtrl',
+			size: 'lg'
+		});
+
+		modal.result.then(function(imageUrl) {
+			
+		});
+	}
+
+	$scope.doStep2 = function() {
+
+	}
 
 });
